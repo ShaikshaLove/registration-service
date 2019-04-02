504e39dda826e87999ca7aa9657c78b7e10ace42
ShaikshaLove <44459273+ShaikshaLove@users.noreply.github.com>
2019-04-02 19:25:48 +0530

    Update RegistrationServiceApplication.java

--- src/main/java/io/s3soft/registration/RegistrationServiceApplication.java
+++ src/main/java/io/s3soft/registration/RegistrationServiceApplication.java
@@ -2,6 +2,7 @@ package io.s3soft.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
